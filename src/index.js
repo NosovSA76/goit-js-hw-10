34ac7d51be2de7b5fc763446e3b49a1449142b6b
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-04-05 14:12:11 +0300

    Deleted second fetch

--- src/index.js
+++ src/index.js
@@ -4,29 +4,43 @@ import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
-
+let coumtryList
 const countriesList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 const searchBox = document.querySelector('#search-box');
 
+
 countriesList.style.display = 'none';
 countryInfo.style.display = 'none';
 
+// Записуємо дані введені корсистувачем у змінну value з затримкою в 0,3 сек, при очищені вікна пошуку
+// скриваємо інформацію з попереднього запросу
+
 searchBox.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
-
-
 function onInputSearch(e) {
   const value = searchBox.value.trim();
-  console.log(value);
-
   if (!value) {
     addHidden();
     clearInterfaceUI();
     return;
   }
 
+  function clearInterfaceUI() {
+  countriesList.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
+
+function addHidden() {
+  countriesList.style.display = 'none';
+  countryInfo.style.display = 'none';
+}
+
+  // використовуємо дані які вносить користувач для створення запросу на API сервер у разі якщо отриманий массив більше 10
+  // обєктів виводимо повідомлення з проханням уточнити запрос, якщо отримуємо пустий масив виводимо повідомленні щодо хибного пошуку
+
   fetchCountries(value)
     .then(data => {
+      coumtryList = data
       if (data.length > 10) {
         Notify.info(
           'Too many matches found. Please enter a more specific name.'
@@ -38,6 +52,25 @@ function onInputSearch(e) {
       clearInterfaceUI();
       Notify.failure('Oops, there is no country with that name');
     });
+}
+
+// Якщо в результаті запросу отриман лише 1 один обєкт виводимо інформацію щощдо країни, якщо декілько створюємо перелік
+// зі скороченою інформацією(прапор, назва) у виді кнопок доступних для натискання для уточнення запросу
+
+function renderCountries(result) {
+  if (result.length === 1) {
+    countriesList.innerHTML = '';
+    countriesList.style.display = 'none';
+    countryInfo.style.display = 'block';
+    countryInfo.innerHTML = generateMarkupCountryInfo(result);
+  }
+  if (result.length >= 2 && result.length <= 10) {
+    countryInfo.innerHTML = '';
+    countryInfo.style.display = 'none';
+    countriesList.style.display = 'block';
+    countriesList.innerHTML = generateMarkupCountryList(result);
+    countryButton()
+  }
 }
 
 const generateMarkupCountryInfo = data => {
@@ -77,50 +110,18 @@ const generateMarkupCountryList = data =>
     );
   }, '');
 
-function renderCountries(result) {
-  if (result.length === 1) {
-    countriesList.innerHTML = '';
-    countriesList.style.display = 'none';
-    countryInfo.style.display = 'block';
-    countryInfo.innerHTML = generateMarkupCountryInfo(result);
-  }
-  if (result.length >= 2 && result.length <= 10) {
-    countryInfo.innerHTML = '';
-    countryInfo.style.display = 'none';
-    countriesList.style.display = 'block';
-    countriesList.innerHTML = generateMarkupCountryList(result);
-    countryButton()
-  }
-}
-
-function clearInterfaceUI() {
-  countriesList.innerHTML = '';
-  countryInfo.innerHTML = '';
-}
-
-function addHidden() {
-  countriesList.style.display = 'none';
-  countryInfo.style.display = 'none';
-}
-
-
-// бажано залишити лише виклик функції якій передати окреме значення
 
 function countryButton() {
   const countryButtons = document.querySelectorAll('.country-button');
-  countryButtons.forEach(countryButton => {
+  countryButtons.forEach((countryButton, index) => {
     countryButton.addEventListener('click', () => {
       const buttonText = countryButton.textContent.trim();
-      searchBox.value = buttonText
-
-      // Зайвий код??
-      fetchCountries(buttonText)
-        .then(data => {
-          countryInfo.style.display = 'block';
-          countriesList.style.display = 'none';
-          countryInfo.innerHTML = generateMarkupCountryInfo(data);
-        });
-      // чи можна скоротити??
+      searchBox.value = buttonText;
+      const countryData = coumtryList[index];
+      const countryInfoMarkup = generateMarkupCountryInfo([countryData]);
+      countryInfo.innerHTML = countryInfoMarkup;
+      countriesList.style.display = 'none';
+      countryInfo.style.display = 'block';
     });
-  })
+  });
 }
